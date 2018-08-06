4027a1105808a53a16c5f15aea50138c3b75b30f
harleyjj <harleyyesm@gmail.com>
2018-08-06 10:43:21 -0700

    set up authentication on the server side

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
+const localStrategy = require('./auth/passport-local');
+const jwtStrategy = require('./auth/passport-jwt');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
+
+const usersRouter = require('./users/router');
+const authRouter = require('./auth/router');
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 const app = express();
 
@@ -20,6 +29,31 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+//Parse request body
+app.use(express.json());
+
+//Mount routers
+app.use('/api', usersRouter);
+app.use('/api', authRouter);
+
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 
 function runServer(port = PORT) {
   const server = app
