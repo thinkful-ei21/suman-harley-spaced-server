ec85972877d410f6327712ec4993ef1494891c49
harleyjj <harleyyesm@gmail.com>
2018-08-08 10:57:36 -0700

    implemented put questions endpoint

--- questions/questionRouter.js
+++ questions/questionRouter.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const jwtDecode = require('jwt-decode');
 
 const User = require('../users/model');
+const Question = require('./questionModel');
+const updatedQuestions = require('./questionUpdate');
 
 const router = express.Router();
 
@@ -20,11 +22,54 @@ router.get('/questions', (req, res, next) => {
   if (decodedToken.user) {
     username = decodedToken.user.username;
   }
-  console.log(username);
+  //console.log(username);
   User.findOne({ username })
     .then(result => {
       if (result) {
         res.json(result.question[0]);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.put('/questions', (req, res, next) => {
+  let decodedToken = '';
+  if (req.headers.authorization) {
+    decodedToken = jwtDecode(req.headers.authorization.slice(7));
+  }
+  let username = '';
+  if (decodedToken.user) {
+    username = decodedToken.user.username;
+  }
+  const { answer } = req.body;
+  //console.log(username);
+  User.findOne({ username })
+    .then(result => {
+      if (result) {
+        //console.log(result);
+        return Question.findOne({question: result.question[0].question})
+          .then(q => {
+            let payload = {};
+            if (q.answer === answer) {
+              payload = updatedQuestions(result.question, true);
+            } else {
+              payload = updatedQuestions(result.question, false);
+            }
+            payload.updatedQuestion.answer = q.answer;
+            return payload;
+          });
+      } else {
+        next();
+      }
+    })
+    .then(result => {
+      if (result) {
+        res.json(result.updatedQuestion);
+        return User.findOneAndUpdate({username}, {question: result.newQuestions});
       } else {
         next();
       }
---
+++ questions/questionUpdate.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function updateQuestions(arr, correct) {
+  const newArr = arr.slice(1);
+  arr[0].attempts = arr[0].attempts + 1;
+  if (correct) {
+    arr[0].correct = arr[0].correct + 1;
+  }
+  newArr.push(arr[0]);
+  return {
+    newQuestions: newArr,
+    updatedQuestion: arr[0]
+  };
+}
+
+module.exports = updateQuestions;
