651e88f1211bd39007dee3d82f57a5b1909f06f9
Suman Bandita <sumanbandita@gmail.com>
2018-08-10 10:24:07 -0400

    string to number

--- questions/questionUpdate.js
+++ questions/questionUpdate.js
@@ -2,9 +2,9 @@
 
 function updateQuestions(arr, correct) {
   const newArr = arr.slice(1);
-  arr[0].attempts = arr[0].attempts + 1;
+  arr[0].attempts = parseInt(arr[0].attempts) + 1;
   if (correct) {
-    arr[0].correct = arr[0].correct + 1;
+    arr[0].correct = parseInt(arr[0].correct) + 1;
   }
   newArr.push(arr[0]);
   return {
